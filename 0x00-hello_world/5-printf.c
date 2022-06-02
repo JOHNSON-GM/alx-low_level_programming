2c0926b00b7f0661428cddd99c1977828e2390cf
JOHNSON-GM <gichukijohnson93@gmail.com>
2022-06-02 05:12:23 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+ #include <stdio.h>
 
-int main(void)
+ int main(void)
 
-{
-	printf("with proper grammar, but the outcome is a piece of art,);
-
-		return (0);
-}
+ {
+        	printf("with proper grammar, but the outcome is a piece of art,\n");
+ 
+ 		return (0);
+ }
