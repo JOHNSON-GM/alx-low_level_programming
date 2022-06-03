9dfc198b2d02ccd3f1d94aeb97ea9e8cb868aca2
JOHNSON-GM <gichukijohnson93@gmail.com>
2022-06-03 04:16:30 -0700

    using functions

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 /**
  * main - Entrypoint
  *
@@ -12,7 +11,6 @@ int main(void)
 	int n;
 
 
-	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
