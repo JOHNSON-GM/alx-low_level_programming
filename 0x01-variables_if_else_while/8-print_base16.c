fa51c060d65519c3674fac803b1e53663a91417b
JOHNSON-GM <gichukijohnson93@gmail.com>
2022-06-05 13:45:49 -0700

    printing numbers from 0-9 an letters froma-f

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers 0 to 9 and letters a to f
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int X;
+
+	for (X = 48; X < 58; X++)
+	{
+		putchar(X);
+	}
+	for (X = 97; X < 103; X++)
+	{
+		putchar(X);
+	}
+
+	return (0);
+}
