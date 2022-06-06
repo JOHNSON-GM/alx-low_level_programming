ea07e885e49cd5c39fd3f9ddef62dac15e2e7d0e
JOHNSON-GM <gichukijohnson93@gmail.com>
2022-06-06 05:37:02 -0700

    printing combinations of 3 digit numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - printing combinations of three digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int hund;
+	int tens;
+	int ones;
+
+	for (hund = 0; hund <= 9; hund++)
+	{
+		for (tens = hund + 1; tens <= 9; tens++)
+		{
+			for (ones = tens + 1; ones <= 9; ones++)
+			{
+				putchar(hund + '0');
+				putchar(tens + '0');
+				putchar(ones + '0');
+
+				if (hund < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	return (0);
+}
