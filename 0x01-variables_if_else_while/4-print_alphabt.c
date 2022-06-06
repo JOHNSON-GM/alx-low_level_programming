f45c81a07d855bc7e545c7ea06aae2596841b227
JOHNSON-GM <gichukijohnson93@gmail.com>
2022-06-06 01:33:32 -0700

    printing alphabet in lowercase

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,16 +11,13 @@
 int main(void)
 {
 	int x;
+
 	for (x = 97; x <= 122; x++)
-
 		if (x != 101 && x != 113)
-		
-		{
-	
-			putchar(x);
-		}
-
-			putchar('\n');
+	{
+		putchar(x);
+	}
+	putchar('\n');
 
 
 	return (0);
