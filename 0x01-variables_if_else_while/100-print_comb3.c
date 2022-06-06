4ba71f755cd89c993ba93a3066387e34466a9cd7
JOHNSON-GM <gichukijohnson93@gmail.com>
2022-06-06 04:27:09 -0700

    printing combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,15 +18,15 @@ int main(void)
 	
 	for (tens = 0; tens <= 9; tens++)
 	{
-		for (ones = tens + 1; ones <= 9; ones++)
+	for (ones = tens + 1; ones <= 9; ones++)
 	}
 	putchar(tens + '0');
 	putchar(ones + '0');
 	
 	if (tens < 8)
 	{
-		putchar(',');
-		putchar(' ');
+	putchar(',');
+	putchar(' ');
 	}
 	}
 	}
