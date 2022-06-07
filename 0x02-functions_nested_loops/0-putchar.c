43433cb2feed3ca99d247017418ae6215e1b8dca
JOHNSON-GM <gichukijohnson93@gmail.com>
2022-06-07 12:50:59 -0700

    printing putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar, followed by anew line
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char sh[8] = "_putchar";
+	int x;
+
+	for (x = 0; x < 8; x++)
+	{
+		_putchar(sh[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a project based on nested loops and functions in c programming.
