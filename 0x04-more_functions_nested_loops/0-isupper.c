0163e810c22aa446039d3cd40b636fda30c8fe22
JOHNSON-GM <gichukijohnson93@gmail.com>
2022-06-09 01:43:22 -0700

    a function that checks for uppercase character.

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,11 +3,12 @@
 /**
  * _isupper - checks if letter is uppercase or lowercase
  * Description: returns 1 when uppercase and 0 when lowercase
+ * c - parameter to be used
  * Return: ALways 0 (success)
  */
 int _isupper(int c);
 {
-	for(c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		for (c = 'A'; c <= 'Z')
 		{
