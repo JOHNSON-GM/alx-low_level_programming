3b66a8d27267449034667f8272cc4236fa3f29aa
JOHNSON-GM <gichukijohnson93@gmail.com>
2022-06-09 18:16:44 -0700

    a function that prints a triangle, followed by a new line.

--- 0x04-more_functions_nested_loops/10-main.c
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -16,13 +16,13 @@ int main(void)
 
 {
 
-    print_triangle(2);
+    print_line(2);
 
-    print_triangle(10);
+    print_line(10);
 
-    print_triangle(1);
+    print_line(1);
 
-    print_triangle(0);
+    print_line(0);
 
     return (0);
 
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 
-#include "holberton.h"
+#include "main.h"
 
 
 
