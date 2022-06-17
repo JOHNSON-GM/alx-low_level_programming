7108989107ea2a44c06c12febdbfc760467918d7
JOHNSON-GM <gichukijohnson93@gmail.com>
2022-06-17 13:48:25 -0700

    a function that adds two numbers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,54 +11,37 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int x, y, k, l, m, n;
+int i, j, k, l, m, n;
 
-for (x = 0; n1[x]; x++)
-for (y = 0; n2[y]; y++)
-
-if (x > size_r || y > size_r)
+for (i = 0; n1[i]; i++)
+;
+for (j = 0; n2[j]; j++)
+;
+if (i > size_r || j > size_r)
 return (0);
 m = 0;
-for (x -= 1, y -= 1, k = 0; k < size_r - 1; x--, y--, k++)
+for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 {
 n = m;
-if (x >= 0)
-n += n1[x] - '0';
-if (y >= 0)
-n += n2[y] - '0';
-
-if (x < 0 && j < 0 && n == 0)
-
+if (i >= 0)
+n += n1[i] - '0';
+if (j >= 0)
+n += n2[j] - '0';
+if (i < 0 && j < 0 && n == 0)
 {
-
 break;
-
 }
-
 m = n / 10;
-
 r[k] = n % 10 + '0';
-
 }
-
 r[k] = '\0';
-
-if (x >= 0 || y >= 0 || m)
-
+if (i >= 0 || j >= 0 || m)
 return (0);
-
 for (k -= 1, l = 0; l < k; k--, l++)
-
 {
-
 m = r[k];
-
 r[k] = r[l];
-
 r[l] = m;
-
 }
-
 return (r);
-
 }
