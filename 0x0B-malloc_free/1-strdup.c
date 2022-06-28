203e328ea3fb4c420d9cdfb7dfcc2715e2fc348e
JOHNSON.GM <gichukijohnson93@gmail.com>
2022-06-28 23:29:44 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,7 +13,10 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; str[i]; i++)
-		s = malloc(sizeof(char) * i);
+		i++;
+	
+	s = malloc(sizeof(char) * i);
+	
 	if (s = NULL)
 	{
 		return (NULL);
