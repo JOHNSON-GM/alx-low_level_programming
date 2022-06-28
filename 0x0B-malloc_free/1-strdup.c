8f3016a891631edbe8ce6720767894ecb82b958e
JOHNSON.GM <gichukijohnson93@gmail.com>
2022-06-28 23:48:30 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,13 +5,15 @@
  * @str: pointer to string
  * Return: pointer
  */
+
 char *_strdup(char *str)
 {
 	char *s;
 	int i = 0, j;
 
-	if (str == NULL)
+	if (!str)
 		return (NULL);
+
 	while (*(str + i))
 		i++;
 	i++;
@@ -21,11 +23,10 @@ char *_strdup(char *str)
 	{
 		return (NULL);
 	}
-		for (j = 0; j <= i; j++);
+
+	for (j = 0; j <= i; j++)
 	{
 		s[j] = str[j];
 	}
 	return (s);
 }
-
-
