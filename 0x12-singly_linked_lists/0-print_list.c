7c0365bd4ce55abbbc99577f9e57b9ea67428f7d
JOHNSON.GM <gichukijohnson93@gmail.com>
2022-07-15 02:20:44 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,8 +7,8 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t nelem;
-nelem = 0;
+size_t x;
+x = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
@@ -16,7 +16,7 @@ printf("[%d] %s\n", 0, "(nil)");
 else
 printf("[%d] %s\n", h->len, h->str);
 h = h->next;
-nelem++;
+x++;
 }
-return (nelem);
+return (x);
 }
