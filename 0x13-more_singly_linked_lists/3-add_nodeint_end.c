951c147a41f1bffb468b5d068d07ec7b31abc0c2
JOHNSON-GM <gichukijohnson93@gmail.com>
2022-07-20 00:33:59 -0700

    fixed betty

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,11 +3,11 @@
  * add_nodeint_end - Function
  *
  * Description: adds a new node at the end of a listint_t list
- * 
+ *
  * @head: pointer to pointer of type listint_t
  * @n: constant parameter of type int
  *
- * Return: returns address to created node 
+ * Return: returns address to created node
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
