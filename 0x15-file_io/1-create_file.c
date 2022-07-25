c1e393c0e85bbd73f7f9feabd9c7899313c11c47
JOHNSON-GM <gichukijohnson93@gmail.com>
2022-07-25 06:50:03 -0700

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,16 +9,11 @@
 
 int create_file(const char *filename, char *text_content)
 {
-
 	int o, w, num = 0;
-
-
 
 	if (filename == NULL)
 
 		return (-1);
-
-
 
 	if (text_content != NULL)
 
@@ -30,24 +25,15 @@ int create_file(const char *filename, char *text_content)
 
 	}
 
-
-
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	w = write(o, text_content, num);
-
-
 
 	if (o == -1 || w == -1)
 
 		return (-1);
 
-
-
 	close(o);
-
-
-
 	return (1);
 
 }
